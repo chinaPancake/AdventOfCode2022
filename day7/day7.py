52dbd3ff3532772b3f03ad006ca6aee71c9aa69a
ChinaPancake <mateusz.placek99@gmail.com>
2022-12-08 02:32:32 +0100

    day7

--- day7/day7.py
+++ day7/day7.py
@@ -1,31 +1,53 @@
+from collections import defaultdict
+
+
 f = open('input.txt','r').readlines()
 
 
 #max size of 100000
 #sum of the all directory with maxsize of 100000
 
-commands = [['$ cd /']]
-directory_list = [
-        ]
-asd = '$ cd /'
+# pomysły na rozwiązanie, 
+# 1. Zrobienie dicta który reaguje na komendy // problemy: jak rozwiązać sprawę z przejściem do stworzonego folderu, 
+# po komendzie cd folder tworzymy nową liste do której później appendujemy podfoldery, i wage plików jako int.
+# jak poruszamy sie po listach list? 
+# tworzenie nowego dicta jako folder? 
 
-directory_dict={
-        '$ cd /': Node()
-        }
+#2. Stworzenie klasy folder, która wywołuje się po komendzie cd
 
-if asd == directory[0]:
-    print('dziala')
-for i in f:
-    b = ''.join(i).split('\n')
-    if b[0] == directory[0]:
-        directory_list.dict[b[0][:-1]]
-        print(directory.index(b[0]))
-    if b[0][0:3] == 'dir':
-        directory_list.append([b[0][4:-1]])
+directory = [['$ cd /']]
+directory_list = []
+file_size_dict = defaultdict(int)
+file_size = 0
+make_dir = []
 
-print(directory_list)
+#part two
+needed_disc_space = 70000000 - 30000000
+
+for commands in f:
+    cmd = commands.strip().split()
+    if cmd[1] == 'cd':
+        if cmd[2] == '..':
+            make_dir.pop()
+        else:
+            make_dir.append(cmd[2])
+    else:
+        try:
+            file_size = int(cmd[0])
+            for i in range(len(make_dir)+1):
+                file_size_dict['/'.join(make_dir[:i])] += file_size
+        except:
+            pass
+
+total_used_space = file_size_dict['/']
+to_delete = total_used_space - needed_disc_space
+print(total_used_space)
 
 
-
-head = Node('/')
-dd_dir = Node('nazwa' parent='/')
+ans = 99999999999999999
+for key,value in file_size_dict.items():
+    if value >=to_delete:
+        ans = min(ans,value)
+print(ans)
+#    if value <= 100000:
+#        ans += value
