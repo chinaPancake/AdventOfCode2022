7853d0da60e44a80026cd3fbd334e57d9748f008
ChinaPancake <mateusz.placek99@gmail.com>
2022-12-11 00:14:16 +0100

    day9

--- day9/day9.py
+++ day9/day9.py
@@ -1,6 +1,5 @@
 from re import L
 
-
 f = open('input.txt', 'r').read().strip()
 
 lines = [x for x in f.split('\n')]
@@ -9,7 +8,14 @@ tx,ty = 0,0
 t_positions = set()
 t_positions.add((0,0))
 ans = 0
-zmiena = {(1,1),(1,0),(-1,-1),(0,-1)}
+tail_list = [(0,0) for _ in range(10)]
+
+def tail_function(hx,hy,tx,ty):
+    
+    if abs(hx-tx)>1 or abs(hy-ty)>1:
+        ty +=(hy>ty) - (hy<ty)
+        tx += (hx>tx) - (hx<tx)
+    return tx, ty
 
 for cmd in lines:
     where, amount = cmd.split()
@@ -22,54 +28,16 @@ for cmd in lines:
             hy += 1
         elif where == 'D':
             hy -= 1        
-       
-        if (hx - tx)>1 and (hy == ty): 
-            tx = tx + 1
-
-        elif (hy-ty)>1 and (hx == tx):
-            ty = ty + 1
-
-        elif (tx-hx)>1 and (hy==ty):
-            tx = tx - 1 
-
-        elif (ty-hy)>1 and (tx==hx):
-            ty = ty - 1 
-
-        elif (hx-tx)>1 and (hy-ty)>0:
-            tx = tx + 1
-            ty = ty + 1  
-
-        elif (hy-ty)>1 and (hx-tx)>0:
-            ty = ty + 1
-            tx = tx + 1
-
-        elif (ty-hy)>1 and (tx-hx)>0:
-            ty = ty - 1
-            tx = tx -1
-            
-        elif (tx-hx)>1 and (ty-hy)>0:
-            tx = tx - 1
-            ty = ty -1 
         
-        elif (hx-tx)>1 and (ty-hy)>0:
-            tx = tx + 1 
-            ty = ty - 1
+        tail_list[0] = (hx,hy)
+        for i in range(1, len(tail_list)):
+            tx, ty = tail_function(hx = tail_list[i-1][0],hy = tail_list[i-1][1],tx=tail_list[i][0],ty=tail_list[i][1])
+            tail_list[i] = (tx,ty)
         
-        elif (ty-hy)>1 and (hx-tx)>0:
-            ty = ty - 1
-            tx = tx + 1 
-        elif (tx-hx)>1 and (hy-ty)> 0:
-            tx = tx-1
-            ty = ty +1
-        elif (hy-ty)>1 and (tx-hx)>0:
-            ty = ty+1 
-            tx = tx-1 
+        t_positions.add(tail_list[-1])
 
 
-
-        t_positions.add((tx,ty))
-
-    print(tx,ty,hx,hy)    
+    #print(tx,ty,hx,hy)    
 print(len(t_positions))
 
 
